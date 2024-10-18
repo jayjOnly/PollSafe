b31ac6c23d6ed6ba1f0fdaaa33b456d193c0f63c
jayjOnly <andrewnicholasjansen@gmail.com>
2024-10-18 17:25:22 +0700

    fix: bug routing

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrganizationsController;
 use App\Http\Controllers\OrganizationMemberController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::fallback(function () {
+    return redirect('/');
+});
 
 Route::get('/', function () {
     return view('home');
@@ -24,15 +17,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::get('/organizations/create', [OrganizationsController::class, 'create'])->name('organizations.create');
-Route::post('/organizations', [OrganizationsController::class, 'store'])->name('organizations.store');
-Route::get('/organizations/{organization:uuid}', [OrganizationsController::class, 'show'])->name('organizations.show');
-Route::get('/organizations/{organization:uuid}/edit', [OrganizationsController::class, 'edit'])->name('organizations.edit');
-Route::get('/organizations/{organization:uuid}/manage', [OrganizationsController::class, 'manage'])->name('organizations.manage');
-Route::get('/organizations', [OrganizationsController::class, 'index'])->name('organizations.index');
-Route::put('/organizations/{organization:uuid}', [OrganizationsController::class, 'update'])->name('organizations.update');
-
 Route::middleware(['auth'])->group(function () {
+    Route::get('/organizations/create', [OrganizationsController::class, 'create'])
+        ->name('organizations.create');
+    Route::post('/organizations', [OrganizationsController::class, 'store'])
+        ->name('organizations.store');
+    Route::get('/organizations/{organization:uuid}', [OrganizationsController::class, 'show'])
+        ->name('organizations.show');
+    Route::get('/organizations/{organization:uuid}/edit', [OrganizationsController::class, 'edit'])
+        ->name('organizations.edit');
+    Route::get('/organizations/{organization:uuid}/manage', [OrganizationsController::class, 'manage'])
+        ->name('organizations.manage');
+    Route::get('/organizations', [OrganizationsController::class, 'index'])
+        ->name('organizations.index');
+    Route::put('/organizations/{organization:uuid}', [OrganizationsController::class, 'update'])
+        ->name('organizations.update');
     Route::get('/organizations/{organization}/members', [OrganizationMemberController::class, 'index'])
         ->name('organizations.members.index');
     Route::post('/organizations/{organization}/members/invite', [OrganizationMemberController::class, 'invite'])
@@ -55,9 +54,16 @@ Route::get('/test-page', function () {
     return view('test-page');
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->group(function () {
+
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1'); // Batasi 5 request per menit
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:5,1'); // Batasi 5 request per menit
+});
+
+Route::middleware('auth')->group(function () {
+    // Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
